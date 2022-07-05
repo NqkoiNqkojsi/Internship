129e65a1fd91cdba7fb1f773ecd6deab094e53bf
Stelian Grozev <tanya_dzh@abv.bg>
2022-07-05 21:20:07 +0300

    binding works

--- internshipProj/GovAnalysis/views.py
+++ internshipProj/GovAnalysis/views.py
@@ -3,6 +3,8 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 from GovAnalysis.models import Articles, EntitiesInArticle
 from django.template import Context, Template
+import sqlite3
+
 
 # Create your views here.
 def index(request):
@@ -11,6 +13,16 @@ def index(request):
 
 def article(request, id):
     row = Articles.objects.all()
-    return render(request, 'article.html')
+    conn = sqliteConnection = sqlite3.connect('../articles.db')
+    try:
+        cursor = conn.cursor()
+        cursor.execute("select * from articlemodel where id="+str(id))
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+            context=dict({"imgUrl":row[3], "titleCont":row[5], "bodyCont":row[6]})
+    finally:
+        conn.close()
+    return render(request, 'article.html', context)
 
 
