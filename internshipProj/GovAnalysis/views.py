f86f68dfa14435f4f4813ffb1319d945732a585e
Stelian Grozev <tanya_dzh@abv.bg>
2022-07-06 02:44:10 +0300

    new article list working somewhat

--- internshipProj/GovAnalysis/views.py
+++ internshipProj/GovAnalysis/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from GovAnalysis.models import Articles, EntitiesInArticle
 from django.template import Context, Template
 from django.core.paginator import Paginator
+from datetime import datetime
 import sqlite3
 
 
@@ -27,6 +28,7 @@ def Article(request, id):
 
 
 def ListArticle(request, page):
+    numb=[page, page+1, page+2, page+3, page+4]
     conn = sqliteConnection = sqlite3.connect('../articles.db')
     try:
         cursor = conn.cursor()
@@ -35,9 +37,19 @@ def ListArticle(request, page):
     finally:
         conn.close()
     
-    paginator = Paginator(rows, 20) # Show 25 contacts per page.
+    art_lenght=len(rows)
+    # TO DO Sort
+    paginator = Paginator(rows, 10) # Show 25 contacts per page.
     page_number = page
     page_obj = paginator.get_page(page_number)
-    return render(request, 'articlesList.html')
+    numb4=page+4
+    if art_lenght==numb4:
+        numb4=-1
+    context=dict({"art":page_obj,
+     "numb0":page, "numb1":page+1, "numb2":page+2, "numb3":page+3, "numb4":numb4})
+    return render(request, 'articlesList.html', context)
 
 
+def SortArticles(rows):
+    #datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
+    pass
